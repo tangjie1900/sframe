3a080be4f3aa111aad9eb1232c8a6a5f79ef7a3c
tangjie <1078399638@qq.com>
2018-06-25 17:00:49 +0800

    change

--- src/main/java/cn/cout/pure/sframe/controller/HelloController.java
+++ src/main/java/cn/cout/pure/sframe/controller/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
 	@RequestMapping("/hello")
 	public String hello() {
 		return "2222";
