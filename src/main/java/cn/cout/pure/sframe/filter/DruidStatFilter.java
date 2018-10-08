e0258d518bd6a67d6502ab3c6ea5200e18db5385
tangjie <1078399638@qq.com>
2018-10-08 14:27:37 +0800

    修改一些

--- src/main/java/cn/cout/pure/sframe/filter/DruidStatFilter.java
+++ src/main/java/cn/cout/pure/sframe/filter/DruidStatFilter.java
@@ -1,6 +1,5 @@
 package cn.cout.pure.sframe.filter;
 
-import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.support.http.WebStatFilter;
 
 import javax.servlet.annotation.WebFilter;
